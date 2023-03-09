62e354224ee9a3e7294b2e4865e085150b673757
Anderson Correa <andersonfpcorrea@gmail.com>
2023-03-08 22:14:09 -0300

    feat: create place-bid handler

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,39 @@
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import commonMiddleware from '../lib/commonMiddleware';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  let auction;
+
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+  const params = {
+    TableName: process.env.AUCTION_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: 'set highestBid.amount = :amount',
+    ExpressionAttributesValues: {
+      ':amount': amount,
+    },
+    ReturnValues: 'ALL_NEW',
+  };
+
+  let updatedAuction;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (err) {
+    console.error(err);
+    throw new createError.InternalServerError(err);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = commonMiddleware(placeBid);
